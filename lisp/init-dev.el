42c0152f6b9541dfe19d960dada28b94ed51c9c5
lamkakyun <lamkakyun@gmail.com>
2018-08-04 01:43:27 +0800

    no comment

--- lisp/init-dev.el
+++ lisp/init-dev.el
@@ -1,7 +1,7 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; common lisp 开发环境
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(load (expand-file-name "~/Config/quicklisp/slime-helper.el"))
+(load (expand-file-name "/opt/Config/quicklisp/slime-helper.el"))
 ;; Replace "sbcl" with the path to your implementation
 (setq inferior-lisp-program "sbcl")
 (require 'slime)
