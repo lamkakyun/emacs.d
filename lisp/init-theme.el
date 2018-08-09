f2cb40681e47c254f78e833c7c25445fc801bbf9
lamkakyun <lamkakyun@gmail.com>
2018-08-09 11:29:24 +0800

    no comment

--- lisp/init-theme.el
+++ lisp/init-theme.el
@@ -1,3 +1,4 @@
+
 (global-linum-mode t)
 (column-number-mode t)
 (setq inhibit-startup-message t) ;; hide welcome screen
@@ -8,5 +9,21 @@
 (set-default-coding-systems 'utf-8)
 ;; (electric-pair-mode t)
 (load-theme 'klere t)
+;; (tool-bar-mode nil)
+
+(setq default-directory "~/.emacs.d")
+
+;; 改变 Emacs 固执的要你回答 yes 的行为。按 y 或空格键表示 yes
+(fset 'yes-or-no-p 'y-or-n-p)
+;; (setq column-number-mode t)
+;; (setq line-number-mode t)
+
+;; 设定不产生备份文件
+(setq make-backup-files nil)
+
+;; 不生成临时文件
+(setq-default make-backup-files nil)
+
 
 (provide 'init-theme)
+
--- lisp/keyset.el
+++ lisp/keyset.el
@@ -1,4 +1,8 @@
+
 (defun key-set ()
+
+  ;; mac os key settings
+  
   (global-set-key [f8] 'neotree-toggle)
   (global-set-key (kbd "C-c C-j") 'direx:jump-to-directory)
   (global-set-key (kbd "C-s") 'swiper)
